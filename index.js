abb49b46240cd34d9abfc613d8721c2dbd10c411
Krishan Shamod <krishanshamod@gmail.com>
2023-02-10 14:29:53 +0530

    Updated the index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Simple Web Application v1.7');
+  res.send('Simple Web Application v1.8');
 });
 
 app.listen(8081, function () {
